b60b7cd5c47e2cd8cd4b8112a82f7e894b2c4c9c
rumman321 <cset-210652@dti.ac>
2025-03-14 02:15:28 +0600

    navber complete

--- BidApp/src/Route/Routes.jsx
+++ BidApp/src/Route/Routes.jsx
@@ -3,10 +3,17 @@ import {
     RouterProvider,
   } from "react-router-dom";
 import App from "../App";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home";
 
   export const router = createBrowserRouter([
     {
       path: "/",
-      element: <App></App>,
+      element: <MainLayout></MainLayout>,
+      children:[
+        {path: "/",
+        element: <Home/>
+    }
+      ]
     },
   ]);
