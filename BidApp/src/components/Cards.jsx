5b6f5089b2a44fab81529d92711c879c020336e8
rumman321 <cset-210652@dti.ac>
2025-03-14 03:04:19 +0600

    hero section and latest auction section complete

--- BidApp/src/components/Cards.jsx
+++ BidApp/src/components/Cards.jsx
@@ -4,7 +4,7 @@ import SingleCard from './SingleCard';
 const Cards = () => {
     return (
         <div>
-           <h2 className='font-bold text-center text-5xl pt-10 pb-5'> Latest Auction Buzz</h2>
+           <h2 className='font-bold text-center text-black text-5xl pt-10 pb-5'> Latest Auction Buzz</h2>
         <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
             <SingleCard/>
             <SingleCard/>
