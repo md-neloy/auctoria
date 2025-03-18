42cb05c51571d05f074f9d953ce555ff49e78435
mhneloy <mhneloy708@gmail.com>
2025-03-18 15:24:46 +0600

    create privateRoutes & AdminPrivate Routes component

--- BidApp/src/privateRoutes/AdminRoutes.jsx
+++ BidApp/src/privateRoutes/AdminRoutes.jsx
@@ -1,5 +1,19 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useContextHooks from "../useHooks/useContextHooks";
+import useAdmin from "./useAdmin";
+
 const AdminRoutes = ({ children }) => {
-  return <div></div>;
+  const location = useLocation();
+  const { user, loading } = useContextHooks();
+  const [isAdmin, isAdminLoading] = useAdmin();
+  if (loading || isAdminLoading) {
+    return <span className="loading loading-bars loading-xl"></span>;
+  }
+  if (user && isAdmin) {
+    return children;
+  }
+
+  return <Navigate to={"/login"} state={location.pathname} />;
 };
 
 export default AdminRoutes;
