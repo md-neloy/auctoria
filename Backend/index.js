31242dc3e8d351b68897f81ccce0a71ea5a62a03
salmaakthermim <salmaakthermim112@gmail.com>
2025-03-20 10:41:31 +0600

    test

--- Backend/index.js
+++ Backend/index.js
@@ -40,6 +40,7 @@ async function run() {
 }
 run().catch(console.dir);
 
+
 app.get("/", async (req, res) => {
   res.send("Agro is running");
 });
--- BidApp/src/Route/Routes.jsx
+++ BidApp/src/Route/Routes.jsx
@@ -2,11 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Login from "../Pages/Authentication/Login";
 import Register from "../Pages/Authentication/Register";
-<<<<<<< HEAD
 import AddProduct from "../components/AddProduct";
-=======
 import Home from "../Pages/HomePage/Home";
->>>>>>> origin/main
 
 export const router = createBrowserRouter([
   {
