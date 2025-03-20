fc1cd4b142897f8f1b36add252460370eb30ac39
Bristyakter25 <sabihaakterbristy@gmail.com>
2025-03-20 10:42:23 +0600

    added All Auction Page

---
+++ BidApp/src/Pages/AllAuctions.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import AllAuctionCard from "./HomePage/AllAuctionCard";
+
+
+const AllAuctions = () => {
+    const [allAuctions,setAllAuctions] = useState([]);
+   
+    useEffect(() => {
+        fetch("http://localhost:5000/addProducts")
+          .then((res) => res.json())
+          .then((data) => {
+            setAllAuctions(data);
+            
+            
+          })
+          .catch((error) => {
+            console.error('Error fetching blogs:', error);
+            
+          });
+      }, []);
+
+
+    return (
+        <div>
+            <h2 className="text-center text-3xl text-purple-400 font-semibold my-5">All Auctions</h2>
+            <div >
+                {
+                    allAuctions.map(auction => <AllAuctionCard key={auction._id} auction={auction}></AllAuctionCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default AllAuctions;
