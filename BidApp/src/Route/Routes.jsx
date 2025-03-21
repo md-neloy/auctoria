7737a1454c50fa5069d44810aa61a4173bf5d94d
mhneloy <mhneloy708@gmail.com>
2025-03-21 15:00:06 +0600

    create the boiler plate of dashboard component

--- BidApp/src/Route/Routes.jsx
+++ BidApp/src/Route/Routes.jsx
@@ -2,18 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Login from "../Pages/Authentication/Login";
 import Register from "../Pages/Authentication/Register";
-
-
 import Home from "../Pages/HomePage/Home";
-<<<<<<< HEAD
 import PrivateRoutes from "../privateRoutes/PrivateRoutes";
 import Dashboard from "../Pages/Dashboard/Dashboard";
-=======
-import AllAuctions from "../Pages/AllAuctions";
-import AddProduct from "../Pages/AddProduct";
-import Profile from "../components/HomeComponents/profile";
-
->>>>>>> ef2d09f88561956a1f7b61d5c9ace05d32610d91
 
 export const router = createBrowserRouter([
   {
@@ -32,19 +23,6 @@ export const router = createBrowserRouter([
         path: "/signup",
         element: <Register />,
       },
-      {
-        path:'/addProduct',
-        element:<AddProduct></AddProduct>
-      },
-      {
-        path:'/allAuctions',
-        element:<AllAuctions></AllAuctions>
-      },
-      {
-        path:'/profile',
-        element:<Profile></Profile>
-      },
-
     ],
   },
   {
