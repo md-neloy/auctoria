43decb2eb8317787f8e7bfdf4321314ce1ad2740
mhneloy <mhneloy708@gmail.com>
2025-03-21 16:42:27 +0600

    create the dashboard sideNavbar component depends on user based

---
+++ BidApp/src/Pages/Dashboard/dasboardaNavbar/AdminNavbar.jsx
@@ -0,0 +1,9 @@
+const AdminNavbar = () => {
+  return (
+    <div>
+      <h2>Admin Navbar</h2>
+    </div>
+  );
+};
+
+export default AdminNavbar;
--- BidApp/src/Pages/Dashboard/sidebar/Sidebar.jsx
+++ BidApp/src/Pages/Dashboard/sidebar/Sidebar.jsx
@@ -1,7 +1,42 @@
+import { FaHome, FaUser } from "react-icons/fa";
+import useAdmin from "../../../privateRoutes/useAdmin";
+import useSeller from "../../../privateRoutes/useSeller";
+import AdminNavbar from "../dasboardaNavbar/AdminNavbar";
+import BuyerNavbar from "../dasboardaNavbar/BuyerNavbar";
+import SellerNavbar from "../dasboardaNavbar/SellerNavbar";
+import { NavLink } from "react-router-dom";
+
 const Sidebar = () => {
+  const [isAdmin] = useAdmin();
+  const [isSeller] = useSeller();
   return (
     <div>
-      <h2>Sidebar</h2>
+      <div className="w-full md:w-64">
+        <ul>
+          {isAdmin ? (
+            <AdminNavbar />
+          ) : isSeller ? (
+            <SellerNavbar />
+          ) : (
+            <BuyerNavbar />
+          )}
+        </ul>
+        {/* horizontal line */}
+        <div className="divider"></div>
+        {/* share common Navlink */}
+        <ul className="menu">
+          <li>
+            <NavLink to={"/"}>
+              <FaHome /> Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={""}>
+              <FaUser /> Profile
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
